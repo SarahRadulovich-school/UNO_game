97ddfd8d547dd9e95a9205627e1e2a0c68de0dd2
Hussamjh <hussamhaidara@gmail.com>
2019-11-27 15:43:59 -0500

    we deleted player file and we renamed  player1 into player

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,48 +1,47 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package ca.sheridancollege.project;
 
-/**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- * @author dancye, 2018
- */
-public abstract class Player 
-{
-    private String playerID; //the unique ID for this player
-    
-    /**
-     * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player.
-     */
-    public Player(String name)
-    {
-        playerID= name;
-    }
-    
-    /**
-     * @return the playerID
-     */
-    public String getPlayerID() 
-    {
-        return playerID;
-    }
+import java.util.ArrayList;
 
-    /**
-     * Ensure that the playerID is unique
-     * @param givenID the playerID to set
-     */
-    public void setPlayerID(String givenID) 
-    {
-        playerID = givenID;
-    }
-    
-    /**
-     * The method to be instantiated when you subclass the Player class
-     * with your specific type of Player and filled in with logic to play your game.
-     */
-    public abstract void play();
-    
+/**
+ *
+ * @author hussamhaidara
+ */
+public class Player {
+	//Attributes
+	private String playerName;
+	private int handSize;
+	private ArrayList<Card> hand;
+	
+	// Constructor 
+	public Player(){
+		
+	}
+	//getters method
+	public String getPlayerName(){
+		return playerName;
+	}
+	public int getHandSize(){
+		return handSize;
+	}
+	public  ArrayList<Card> getHand(){
+		return hand;
+	}
+	//setter methods all setter dont return anything
+	// set the player name of this plaayerto the value provided in the argument
+	public void setPlayerName(String playerName){
+	this.playerName = playerName;	
+	}
+	public void setHandSize(int handSize){
+	this.handSize = handSize;
+	}
+	//is a list that contain an object of type card
+	public void setHand(ArrayList<Card> hand){
+		this.hand = hand;
+	}
+	
 }
