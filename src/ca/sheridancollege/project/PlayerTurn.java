796278f1270de88d5c4b90de091a1c6deb3ae658
Hussamjh <hussamhaidara@gmail.com>
2019-11-27 15:59:44 -0500

    we added a new class called Deck

---
+++ src/ca/sheridancollege/project/PlayerTurn.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author hussamhaidara
+ */
+public class PlayerTurn {
+	
+private Player player;
+private String playerChoice;
+
+public PlayerTurn(){
+	
+}
+public void pickUpTwoCards(){
+	
+}
+public void playerWon(){
+	
+}
+public void UnoOrContinue(){
+	
+}
+public void cardDecision(){
+	
+}
+public void pickUpOneCard(){
+	
+}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public String getPlayerChoice() {
+		return playerChoice;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public void setPlayerChoice(String playerChoice) {
+		this.playerChoice = playerChoice;
+	}
+
+}
