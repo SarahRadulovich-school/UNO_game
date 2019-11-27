b7e594c64f4c4eec12f6fd20325d83074b8a4fdc
Hussamjh <hussamhaidara@gmail.com>
2019-11-27 15:30:37 -0500

    We added CardType.java,and  Colour.java

---
+++ src/ca/sheridancollege/project/CardType.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author hussamhaidara
+ */
+public enum CardType {
+	ZERO, ONE, TWO, THREE,
+	FOUR, FIVE, SIX, SEVEN,
+	EIGHT, NINE, PICKUP2, PICKUP4,
+	REVERSE, SKIPTURN, WILDCARD
+	
+}
