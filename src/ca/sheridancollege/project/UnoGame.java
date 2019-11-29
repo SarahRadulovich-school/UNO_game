c947ef4a950dc64bc61f30b311a8c1e7b109389f
SarahRadulovich-school <radulovs@sheridancollege.ca>
2019-11-28 20:26:52 -0500

    Added user input to main class. Fixed typos.

--- src/ca/sheridancollege/project/UnoGame.java
+++ src/ca/sheridancollege/project/UnoGame.java
@@ -6,6 +6,7 @@
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -19,44 +20,66 @@ public class UnoGame  {
 
 		
 //attributes 
-private Player winnerl;
+private Player winner;
 private ArrayList<Player> players;
-private ArrayList<Player>loosers;
+private ArrayList<Player>losers;
 
 //construstor to instaliz an object
 public UnoGame(){
 	
 }
 	public static void main(String[] args) {
-	
-	}
-	public void newGame(){
-		
-	}
-	//Getters
+            
+            UnoGame uno = new UnoGame();
+            
+            System.out.println("Welcome to UNO!");
+            
+            //Creating 4 Player objects in the ArrayList to represent the 4 players of the game
+            //This program REQUIRES 4 players exactly
+            uno.players = new ArrayList<Player>();
+            for (int i = 0; i < 4; i++) {
+                uno.players.add(new Player());
+            }
 
-	public Player getWinnerl() {
-		return winnerl;
+            //Taking user input for the names of the 4 players
+            Scanner scan = new Scanner(System.in);
+            for (int i = 0; i < 4; i++) {
+                System.out.println("Please enter Player " + (i+1) + "'s name: ");
+                uno.players.get(i).setPlayerName(scan.next());
+            }
+            
+            //starting the game
+            uno.newGame();
+            System.out.println("Thanks for playing!");
+	}
+        
+	public void newGame(){
+	}
+        
+	//getters
+	public Player getWinner() {
+		return winner;
 	}
 
 	public ArrayList<Player> getPlayers() {
 		return players;
 	}
 
-	public ArrayList<Player> getLoosers() {
-		return loosers;
+	public ArrayList<Player> getLosers() {
+		return losers;
 	}
 
-	public void setWinnerl(Player winnerl) {
-		this.winnerl = winnerl;
+        //setters
+	public void setWinner(Player winnerl) {
+		this.winner = winner;
 	}
 
 	public void setPlayers(ArrayList<Player> players) {
 		this.players = players;
 	}
 
-	public void setLoosers(ArrayList<Player> loosers) {
-		this.loosers = loosers;
+	public void setLosers(ArrayList<Player> loosers) {
+		this.losers = loosers;
 	}
 	
 }
