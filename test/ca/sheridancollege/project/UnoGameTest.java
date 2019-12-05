94c6a71bbb5918fd3dfbe525e9acb37f5340821a
Sarah Radulovich <sarah.j.radulovich@gmail.com>
2019-12-04 19:13:49 -0500

    - Deleted CardTypeTest and ColourTest because we don't have anything in them to test - Added tests for the UnoOrContinue method. Not sure if the user input will work though.

--- test/ca/sheridancollege/project/UnoGameTest.java
+++ test/ca/sheridancollege/project/UnoGameTest.java
@@ -12,6 +12,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.*;
 
 /**
  *
@@ -37,31 +38,112 @@ public class UnoGameTest {
     @After
     public void tearDown() {
     }
-
-    /**
-     * Test of main method, of class UnoGame.
-     */
+    
+    //this is so we can simulate input from the user!
+    @Rule
+    public final TextFromStandardInputStream systemInMock
+        = emptyStandardInputStream();
+    
     @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        UnoGame.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testwildCardGood() {
+        System.out.println("wildCardGood");
+        assertEquals(true, pile.validPlay(wildcard));
+    }
+        
+    @Test
+    public void testwildCardBad() {
+        System.out.println("wildCardBad");
+        assertEquals(true, pile.validPlay(wildcard));
+    }
+       
+    @Test
+    public void testwildCardBoundary() {
+        System.out.println("wildCardBoundary");
+        assertEquals(true, pile.validPlay(wildcard));
+    }
+    
+    @Test
+    public void testUnoOrContinueGood() {
+        System.out.println("testUnoOrContinueGood");
+        //The user must select "UNO", since they have one card left
+        UnoGame uno = new UnoGame();
+        Player player = new Player();
+        player.setHandSize(1);
+        
+        ArrayList<Card> hand = new ArrayList();
+        Card card = new Card();
+        card.setColour(Colour.RED);
+        card.setValue(CardType.ZERO);
+        
+        hand.add(card);
+        //the player has a hand with only one card: RED ZERO
+        player.setHand(hand);
+        
+        //the player selects UNO
+        boolean penalty = uno.UnoOrContinue(player);
+        //this is so we can simulate input from the user!
+        systemInMock.provideLines("1");
+        
+        //They should not receieve a penalty, therefore the method should return false
+        assertEquals(false, penalty);
+    }
+        
+    @Test
+    public void testUnoOrContinueBad() {
+        System.out.println("testUnoOrContinueBad");
+        //The user should select "UNO" but instead they select "Continue"
+        UnoGame uno = new UnoGame();
+        Player player = new Player();
+        player.setHandSize(1);
+        
+        ArrayList<Card> hand = new ArrayList();
+        Card card = new Card();
+        card.setColour(Colour.RED);
+        card.setValue(CardType.ZERO);
+        
+        hand.add(card);
+        //the player has a hand with only one card: RED ZERO
+        player.setHand(hand);
+        
+        //the player selects Continue
+        boolean penalty = uno.UnoOrContinue(player);
+        //this is so we can simulate input from the user!
+        systemInMock.provideLines("2");
+        
+        //They should receive the penalty
+        assertEquals(true, penalty);
+    }
+       
+    @Test
+    public void testUnoOrContinueBoundary() {
+        System.out.println("testUnoOrContinueBoundary");
+        //The user will say "UNO" but they do not have 1 card left
+        UnoGame uno = new UnoGame();
+        Player player = new Player();
+        player.setHandSize(2);
+        
+        ArrayList<Card> hand = new ArrayList();
+        
+        Card card1 = new Card();
+        card1.setColour(Colour.RED);
+        card1.setValue(CardType.ZERO);
+        
+        Card card2 = new Card();
+        card2.setColour(Colour.RED);
+        card2.setValue(CardType.ONE);
+        
+        hand.add(card1);
+        hand.add(card2);
+        
+        //the player has a hand with two cards, RED ZERO and RED ONE
+        player.setHand(hand);
+        
+        boolean penalty = uno.UnoOrContinue(player);
+        //this is so we can simulate input from the user!
+        systemInMock.provideLines("1");
+        
+        //Although the player should not have said UNO, they receive no penalty, and the method should return false
+        assertEquals(false, penalty);
     }
 
-    /**
-     * Test of newGame method, of class UnoGame.
-     */
-    @Test
-    public void testNewGame() {
-        System.out.println("newGame");
-        UnoGame instance = new UnoGame();
-        instance.newGame();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    /**
-     * Test of setPlayers method, of class UnoGame.
-     */
 }
